7537dcb743e1c1b45f9294ea844a8cd6a5a79971
Mark Bolwell <mark.bollyuk@gmail.com>
2023-09-22 10:03:56 +0100

    updated with allowlist
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- main.tf
+++ main.tf
@@ -81,14 +81,14 @@ resource "local_file" "inventory" {
         amzn2023cis_rule_1_2_4: false  # Breaks patching
         amzn2023cis_rule_4_6_6: false  # default image has no root password and nopasswd not removed from sudo
         rhel_07_010340: false
-        rhel7stig_bootloader_password_hash: 'grub.pbkdf2.sha512.somethingnewhere'
+        rhel7stig_bootloader_password_hash: 'grub.pbkdf2.sha512.somethingnewhere'  # pragma: allowlist secret
         rhel9cis_rule_5_6_6: false  # skip root passwd check and keys only
         ## Passwds for ubuntu
-        grub_user_pass: 'grub.pbkdf2.sha512.10000.D268F2334B417C788C859A1104D489BE73205AFB74539DCAB0AC3F4A3B2ADE34D994D6D86A6F665200608F88050BCBC5D161ED07DE78C39D3C2BAE345F22DCEE.730C7E0F06BBDD2A54FF7BE93B710E94E1B1B61FE8E0BF27313E2429AF2C57348BF2EA647E39EF5AB13BE3EF3B1972FA5082EEB62AB9436314EA851D8042F423'
+        grub_user_pass: 'grub.pbkdf2.sha512.10000.D268F2334B417C788C859A1104D489BE73205AFB74539DCAB0AC3F4A3B2ADE34D994D6D86A6F665200608F88050BCBC5D161ED07DE78C39D3C2BAE345F22DCEE.730C7E0F06BBDD2A54FF7BE93B710E94E1B1B61FE8E0BF27313E2429AF2C57348BF2EA647E39EF5AB13BE3EF3B1972FA5082EEB62AB9436314EA851D8042F423'  # pragma: allowlist secret
         ubtu20cis_bootloader_password_hash: "{{ grub_user_pass }}"
-        ubtu20cis_root_pw: '$6$m1u7QuCBzmdHhig3$Ss48R6udPO.sISy8XphR2jlLhGqQiLoKkjdqVVU7zsU108oOq25.Bj0BTeafnljaur7iMnQPYXpRCzgXc6o4U1'
+        ubtu20cis_root_pw: '$6$m1u7QuCBzmdHhig3$Ss48R6udPO.sISy8XphR2jlLhGqQiLoKkjdqVVU7zsU108oOq25.Bj0BTeafnljaur7iMnQPYXpRCzgXc6o4U1'  # pragma: allowlist secret
         ubtu22cis_bootloader_password_hash: "{{ grub_user_pass }}"
         ubtu22cis_set_grub_user_pass: true
-        ubtu22cis_grub_user_passwd: '$y$j9T$MBA5l/tQyWifM869nQjsi.$cTy0ConcNjIYOn6Cppo5NAky20osrkRxz4fEWA8xac6'
+        ubtu22cis_grub_user_passwd: '$y$j9T$MBA5l/tQyWifM869nQjsi.$cTy0ConcNjIYOn6Cppo5NAky20osrkRxz4fEWA8xac6'  # pragma: allowlist secret
     EOF
 }
