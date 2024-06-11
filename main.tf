0ebac70bcd59f2dac29bd130a8d4e075e618b36f
Mark Bolwell <mark.bollyuk@gmail.com>
2024-06-11 11:15:13 +0100

    updated vars for amazon2
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- main.tf
+++ main.tf
@@ -77,6 +77,7 @@ resource "local_file" "inventory" {
         run_audit: true
         system_is_ec2: true
         skip_reboot: false
+        amazon2cis_rule_4_5_2_4: false  # Don't set root password
         amzn2023cis_rule_1_2_2: false  # Breaks patching
         amzn2023cis_rule_1_2_4: false  # Breaks patching
         amzn2023cis_rule_4_6_6: false  # default image has no root password and nopasswd not removed from sudo
