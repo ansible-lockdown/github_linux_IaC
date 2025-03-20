9771fc85bbab13b73bade96f4ba1e6b8f50c8980
Mark Bolwell <mark.bollyuk@gmail.com>
2025-03-20 09:33:52 +0000

    added audit_fetch_option
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "local_file" "inventory" {
       vars:
         setup_audit: true
         run_audit: true
+        fetch_audit_output: true
         system_is_ec2: true
         skip_reboot: false
         amazon2cis_rule_4_5_2_4: false  # Don't set root password
