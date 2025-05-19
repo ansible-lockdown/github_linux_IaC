68f8e51e58d79cf210bd4376dd81b2db1bb18f96
Mark Bolwell <mark.bollyuk@gmail.com>
2025-05-19 09:34:20 +0100

    updated with rhel10 vars
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- main.tf
+++ main.tf
@@ -87,6 +87,11 @@ resource "local_file" "inventory" {
         grub_user_pass: 'grub.pbkdf2.sha512.10000.D268F2334B417C788C859A1104D489BE73205AFB74539DCAB0AC3F4A3B2ADE34D994D6D86A6F665200608F88050BCBC5D161ED07DE78C39D3C2BAE345F22DCEE.730C7E0F06BBDD2A54FF7BE93B710E94E1B1B61FE8E0BF27313E2429AF2C57348BF2EA647E39EF5AB13BE3EF3B1972FA5082EEB62AB9436314EA851D8042F423'  # pragma: allowlist secret
         grub_user_passwd: '$y$j9T$MBA5l/tQyWifM869nQjsi.$cTy0ConcNjIYOn6Cppo5NAky20osrkRxz4fEWA8xac6'
         root_passwd: '$6$m1u7QuCBzmdHhig3$Ss48R6udPO.sISy8XphR2jlLhGqQiLoKkjdqVVU7zsU108oOq25.Bj0BTeafnljaur7iMnQPYXpRCzgXc6o4U1'  # pragma: allowlist secret
+        rhel10cis_sudoers_exclude_nopasswd_list: "{{ sudoers_exclude_nopasswd_list }}"
+        rhel10cis_rule_5_6_6: false  # skip root passwd check and keys only
+        rhel10cis_rule_5_2_4: false # skip user password
+        rhel10cis_rule_5_4_2_4: false # v2 skip root password
+        rhel10cis_bootloader_password_hash: grub.pbkdf2.sha512.10000.9306A36764A7BEA3BF492D1784396B27F52A71812E9955A58709F94EE70697F9BD5366F36E07DEC41B52279A056E2862A93E42069D7BBB08F5DFC2679CD43812.6C32ADA5449303AD5E67A4C150558592A05381331DE6B33463469A236871FA8E70738C6F9066091D877EF88A213C86825E093117F30E9E1BF158D0DB75E7581B  # pragma: allowlist secret
         ### Debian variables
         debian11cis_bootloader_password_hash: "{{ grub_user_pass }}"
         debian11cis_set_grub_user_pass: true
